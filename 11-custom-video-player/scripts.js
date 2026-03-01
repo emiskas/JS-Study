a60c579ce7f14ccf3a2cb704f4053078beda6b04
emiskas <emilispaulauskas279@gmail.com>
2026-03-01 17:04:38 +0200

    add: exercise 11 (WIP)

---
+++ 11-custom-video-player/scripts.js
@@ -0,0 +1,27 @@
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    toggle.textContent = this.paused ? '►' : '❚ ❚';
+}
+
+function skip() {
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
